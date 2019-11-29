1ed528d5b3bdcd56ec230735371d609c87f56a8e
K3be <ahmed.13.alkaaby@gmail.com>
2019-11-29 13:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+      return str.charAt(0).toUpperCase()+str.slice(1);
+    }
+    static sanitize(str){
+      return str.replace(/[^A-Za-z0-9 '-]/g, '')
+    }
+    static titleize(str){
+    let exeptions=['the','a','an','but','of','and','for','at','by','from'];
+    let newArr=str.split(' ');
+    let newArr2=[];
+    for(let i=0; i<newArr.length; i++){
+      if(exceptions.includes(newArr[i])){
+         newArr2.push(newArr[i])
+       } 
+       else {
+         newArr2.push(this.capitalize(newArr[i]))
+       }
+}
+return newArr2.join(' ');
+}
 }
